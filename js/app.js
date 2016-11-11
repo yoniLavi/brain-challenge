92b2f88611f02db1610612c97c0d88bb5fbb30a4
Frank Mullen <frankmullen@gmail.com>
2016-11-11 09:47:35 +0000

    Changed Navbar

---
+++ js/app.js
@@ -0,0 +1,13 @@
+angular.module('BrainApp', ['ngRoute', 'RouteControllers']);
+ 
+angular.module('BrainApp').config(function($routeProvider) {
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/accounts/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+});
---
+++ js/controller.js
@@ -0,0 +1,4 @@
+angular.module('RouteControllers', [])
+    .controller('HomeController', function($scope) {
+        $scope.title = "Welcome To Brain Challenge!";
+    });
