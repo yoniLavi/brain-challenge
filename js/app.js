c0e5741cbc8192b061a18c4e4c559248c78a94b3
Frank Mullen <frankmullen@gmail.com>
2016-11-11 17:47:46 +0000

    Memory Box Layout

--- js/app.js
+++ js/app.js
@@ -6,6 +6,18 @@ angular.module('BrainApp').config(function($routeProvider) {
         templateUrl: 'templates/home.html',
         controller: 'HomeController'
     })
+    .when('/memorybox', {
+        templateUrl: 'templates/memorybox.html',
+        controller: 'MemoryController'
+    })
+    .when('/conundrum', {
+        templateUrl: 'templates/conundrum.html',
+        controller: 'ConundrumController'
+    })
+    .when('/boardswap', {
+        templateUrl: 'templates/boardswap.html',
+        controller: 'BoardController'
+    })
     .when('/accounts/register', {
         templateUrl: 'templates/register.html',
         controller: 'RegisterController'
--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,13 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome To Brain Challenge!";
+        $scope.title = "Welcome To Brain Challenge - Brainiacs' Heaven!";
+    })
+    .controller('MemoryController', function($scope) {
+        $scope.title = "MemoryBox";
+    })
+    .controller('ConundrumController', function($scope) {
+        $scope.title = "Conundrum";
+    })
+    .controller('BoardController', function($scope) {
+        $scope.title = "BoardSwap";
     });
