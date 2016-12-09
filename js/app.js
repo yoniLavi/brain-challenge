f54d2cf1d690df88e9175aeabd079b79ee478c2b
Yoni Lavi <yoni@codeinstitute.net>
2016-12-09 03:01:03 +0000

    refactor modal logic to use single controller and single template

--- js/app.js
+++ js/app.js
@@ -1,38 +1,8 @@
 var myApp = angular.module('BrainApp', ['ngRoute', 'RouteControllers', 'ui.bootstrap', 'ngSanitize'])
 
-/* controllers to pass the game instructions a modal box.
-*  I suspect this could be done in a much cleaner way that wouldn't 
-*  necessitate 3 separate templates
-*/
-myApp.controller('demo1Controller', function($scope, $uibModal) {
-    this.uibModal = function() {
-        var uibModalInstance = $uibModal.open({
-            controller: 'ModalController',
-            templateUrl: 'templates/modal1.html'
-        })
-    }
-});
-
-myApp.controller('demo2Controller', function($scope, $uibModal) {
-    this.uibModal = function() {
-        var uibModalInstance = $uibModal.open({
-            controller: 'ModalController',
-            templateUrl: 'templates/modal2.html'
-        })
-    }
-});
-
-myApp.controller('demo3Controller', function($scope, $uibModal) {
-    this.uibModal = function() {
-        var uibModalInstance = $uibModal.open({
-            controller: 'ModalController',
-            templateUrl: 'templates/modal3.html'
-        })
-    }
-});
 // routing configuration
 myApp.config(function($routeProvider) {
- 
+
     $routeProvider.when('/', {
         title: 'Welcome To Brain Challenge!',
         templateUrl: 'templates/home.html',
--- js/controller.js
+++ js/controller.js
@@ -1,25 +1,38 @@
-angular.module('RouteControllers', [])
-    
+angular.module('RouteControllers', ['ui.bootstrap'])
+
     .controller('HomeController', function($scope) {
-        
     })
     .controller('ModalController', function($scope, $uibModalInstance) {
-        $scope.goback = function() {
-            $uibModalInstance.close('goback');
+        $scope.goback = $uibModalInstance.close;
+    })
+    .controller('MemoryController', function($scope, $uibModal) {
+        $scope.scoreBox = "Memory Score";
+        $scope.modalBody = [
+            "Copy the sequence by clicking on the coloured boxes in the same order you're shown.",
+            "The number of boxes in the sequence will increase by 1 every time.",
+            "Try and beat your high score!",
+        ];
+        $scope.openInstructionsModal = function() {
+            $uibModal.open({scope: $scope, controller: 'ModalController', templateUrl: 'templates/modal.html'});
         }
     })
-    .controller('MemoryController', function($scope) {
-       
-        $scope.scoreBox = "Memory Score";
-        
-    })
-    .controller('ConundrumController', function($scope) {
-        
+    .controller('ConundrumController', function($scope, $uibModal) {
         $scope.scoreBox = "Conundrum Score";
-        
+        $scope.modalBody = [
+            "You have 30 seconds to solve the conundrum to make a real word.",
+            "Type your answer in the box before the time is up!",
+        ];
+        $scope.openInstructionsModal = function() {
+            $uibModal.open({scope: $scope, controller: 'ModalController', templateUrl: 'templates/modal.html'});
+        }
     })
-    .controller('BoardController', function($scope) {
-       
-        $scope.scoreBox = "BoardSwap Score"; 
-        
+    .controller('BoardController', function($scope, $uibModal) {
+        $scope.scoreBox = "BoardSwap Score";
+        $scope.modalBody = [
+            "Swap the red and black pieces in the fewest moves possible by moving into the empty space.",
+            "You can only move a piece within 2 spaces into the empty space.",
+        ];
+        $scope.openInstructionsModal = function() {
+            $uibModal.open({scope: $scope, controller: 'ModalController', templateUrl: 'templates/modal.html'});
+        }
     });
